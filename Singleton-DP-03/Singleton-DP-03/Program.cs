b0047c0436a57314e56f9fd9edad3536fc8f5a0e
Bharath Bhushan M <mbharath1983@gmail.com>
2018-03-30 20:02:34 +0530

    Singleton DP - 03
    This project contains code for why we need to use sealed key word on a class for which we want to created single instance accross the application, after having private constructor.

---
+++ Singleton-DP-03/Singleton-DP-03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Singleton_DP_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Singleton s1 = Singleton.GetInstance;
+            s1.PrintDetails("First message");
+
+            Singleton s2 = Singleton.GetInstance;
+            s2.PrintDetails("It is second message");
+
+            Console.WriteLine();
+
+            // SO here it voilates the singlton pattern by creating second instance for singleton.
+            //Singleton.DerivedClass derivedClass = new Singleton.DerivedClass();
+            //derivedClass.PrintDetails("Derived Class");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton-DP-03/Singleton-DP-03/Singleton.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_DP_03
+{
+    //public class DerivedClass : Singleton
+    //{
+
+    //}
+
+    /// <summary>
+    /// Sealed Class will prevent class inheritance
+    /// </summary>
+    sealed class Singleton
+    {
+        //private static int counter = 0;
+
+        //public Singleton()
+        //{
+        //    counter = counter + 1;
+        //    Console.WriteLine(counter);
+        //}
+
+        private static int counter = 0;
+
+        private static Singleton instance = null;
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+
+                return instance;
+            }
+        }
+
+
+        //Private constructor helps us to prevent any extenal instantiation object 
+        private Singleton()
+        {
+            counter = counter + 1;
+            Console.WriteLine(counter);
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        //// In Nested Class we can inherit singleton class if we removed the sealed keyword
+        //public class DerivedClass : Singleton
+        //{
+
+        //}
+    }
+}
