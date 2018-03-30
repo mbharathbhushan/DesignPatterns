4639caa556baafb513936ff9a2a9c277dee63d1a
Bharath Bhushan M <mbharath1983@gmail.com>
2018-03-30 20:35:12 +0530

    Singleton DesingPattern - 04
    Lazy Initialization in Singleton.
    How to use Multithreads in Singleton.
    How to implment a thread safe function in Singleton.

---
+++ Singleton-DP-04/Singleton-DP-04/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_DP_04
+{
+    sealed class Singleton
+    {
+        private static int counter = 0;
+
+        private static Singleton instance = null;
+
+        private static readonly Object obj = new object();
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                // If we have not lock this instantiation code , with multithread multiple instances may be created.
+                // Below Process in called Lazy loading.
+                if (instance == null)
+                {
+                    lock (obj)
+                    {
+                        // Checking multiple time instance null or not called as (Double check Locking).
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter = counter + 1;
+            Console.WriteLine(counter);
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
