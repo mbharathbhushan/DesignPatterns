65431d292fe27c80b9a64e8f0f058a0258bef689
Bharath Bhushan M <mbharath1983@gmail.com>
2018-03-30 19:32:25 +0530

    Singleton Design Pattern Basic Approach
    Kudvenkat - 02 Vedio - SingleTon - Basic Approch

---
+++ Singleton_02/Singleton_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Singleton_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            ////1) Senario
+            //Singleton s1 = new Singleton();
+            //s1.PrintDetails("First message");
+
+            //Singleton s2 = new Singleton();
+            //s2.PrintDetails("It is second message");
+            //Console.ReadLine();
+
+
+            Console.WriteLine("Hello World!");
+
+            Singleton s1 = Singleton.GetInstance;
+            s1.PrintDetails("First message");
+
+            Singleton s2 = Singleton.GetInstance;
+            s2.PrintDetails("It is second message");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton_02/Singleton_02/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_02
+{
+    sealed class Singleton
+    {
+        //private static int counter = 0;
+
+        //public Singleton()
+        //{
+        //    counter = counter + 1;
+        //    Console.WriteLine(counter);
+        //}
+
+        private static int counter = 0;
+
+        private static Singleton instance = null;
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter = counter + 1;
+            Console.WriteLine(counter);
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
