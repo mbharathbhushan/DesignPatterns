1de9bcc6624b75f353350bfdb78ea789d0a2501b
Bharath Bhushan M <mbharath1983@gmail.com>
2018-04-11 19:58:37 +0530

    DataStore Project added to Factory Design Pattern -08 project.
    DataStore Project added to Factory Design Pattern -08 project.

---
+++ FactoryDesignPattern_08/DataStore/dbo/Tables/Employee.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]             INT          IDENTITY (1, 1) NOT NULL,
+    [Name]           VARCHAR (50) NOT NULL,
+    [JobDescription] VARCHAR (50) NOT NULL,
+    [Number]         VARCHAR (50) NOT NULL,
+    [Department]     VARCHAR (50) NOT NULL,
+    [HourlyPay]      DECIMAL (18) NOT NULL,
+    [Bonus]          DECIMAL (18) NOT NULL,
+    [EmployeeTypeID] INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Employee_EmployeeType] FOREIGN KEY ([EmployeeTypeID]) REFERENCES [dbo].[Employee_Type] ([Id])
+);
+
